22082c23c995cbdb2acfdfdb3c80ca86c751d0ed
roshni <roshni.rajawat@talentpod.com>
2022-01-06 21:18:34 +0530

    Register page and controller modified

--- src/main/java/in/ashokit/Controller/SpecializationController.java
+++ src/main/java/in/ashokit/Controller/SpecializationController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.ashokit.entity.Specialization;
@@ -17,6 +19,28 @@ public class SpecializationController {
 	@Autowired
 	private ISpecializationService service;
 
+	/**
+	 * 1. show Register page
+	 */
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "SpecializationRegister";
+	}
+
+	/**
+	 * 2.on submit form data
+	 */
+	@PostMapping("/save")
+	public String saveForm(@ModelAttribute Specialization specialization, Model model) {
+		Long id = service.saveSpecialization(specialization);
+		String message = "Reocord ("+id+") is created";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";
+	}
+
+	/**
+	 * 3. display all Specialization
+	 */
 	@GetMapping("/all")
 	public String viewAll(Model model) {
 		List<Specialization> list = service.getAllSpecializations();
--- src/main/java/in/ashokit/entity/Specialization.java
+++ src/main/java/in/ashokit/entity/Specialization.java
@@ -15,16 +15,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name ="Specialization_tab")
+@Table(name = "Specialization_tab")
 public class Specialization {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name ="spec_id_col")
+	@Column(name = "spec_id_col")
 	private Long id;
-	@Column(name ="spec_code_col")
+	@Column(name = "spec_code_col", length = 10, nullable = false, unique = true)
 	private String specCode;
-	@Column(name ="spec_name_col")
+	@Column(name = "spec_name_col", length = 10, nullable = false, unique = true)
 	private String specName;
-	@Column(name ="spec_note_col")
+	@Column(name = "spec_note_col", length = 250, nullable = false)
 	private String specNote;
 }
